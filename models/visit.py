b3d8730064fc7ca77cdf72d694fc5d5c5acd9f5d
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-07-02 12:05:25 +0530

    Create visit.py

---
+++ models/visit.py
@@ -0,0 +1,39 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    Time,
+    ForeignKey
+)
+
+from database import Base
+
+
+class Visit(Base):
+    __tablename__ = "visits"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    visitor_id = Column(
+        Integer,
+        ForeignKey("visitors.id")
+    )
+
+    host_name = Column(String)
+
+    department = Column(String)
+
+    visit_date = Column(Date)
+
+    check_in = Column(Time)
+
+    check_out = Column(Time)
+
+    status = Column(
+        String,
+        default="Scheduled"
+    )
